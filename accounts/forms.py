447b122fb0c2a1efb480a33017ea7c999eeece8d
LakshmiSB <lakshmisb282003@gmail.com>
2025-03-26 18:54:33 +0530

    vendor profile form + validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-from .models import User
-#from .validators import allow_only_images_validator
+from .models import User,UserProfile
+from .validators import allow_only_images_validator
 
 
 class UserForm(forms.ModelForm):
@@ -21,16 +21,16 @@ class UserForm(forms.ModelForm):
             )
 
 
-# class UserProfileForm(forms.ModelForm):
-#     address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Start typing...', 'required': 'required'}))
-#     profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
-#     cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Start typing...', 'required': 'required'}))
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
     
-#     # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
-#     # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
-#     class Meta:
-#         model = UserProfile
-#         fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
+    latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
 
 #     def __init__(self, *args, **kwargs):
 #         super(UserProfileForm, self).__init__(*args, **kwargs)
@@ -43,3 +43,4 @@ class UserForm(forms.ModelForm):
 #     class Meta:
 #         model = User
 #         fields = ['first_name', 'last_name', 'phone_number']
+
